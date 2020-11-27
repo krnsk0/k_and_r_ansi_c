0312bab12b7d32668ad6041bad77e35afef3919a
krnsk0 <jonathan.kurinsky@ingka.ikea.com>
2020-11-27 17:44:16 -0500

    multifile

---
+++ multifile/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "one.h"
+
+int main()
+{
+
+  printf("in func main\n");
+
+  printf("return value of one(): %c\n", one());
+}
---
+++ multifile/one.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+char one()
+{
+  printf("in func one\n");
+  return 'a';
+}
